9b42ffadd2b3d43070970f1e2559023e45b08b7a
MarceloAndres <mmendozaf@gmail.com>
2023-09-07 04:54:11 -0300

    fix: changed api route name for legibility

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 import { getAllUsers, getUserProfile, login, signUp } from '../controllers/users.controller.js';
 import { authRequire, adminAuthRequire } from '../middleware/auth.middleware.js';
 
-router.get('/user', adminAuthRequire, getAllUsers );  // ADMIN AUTH
+router.get('/usermanage', adminAuthRequire, getAllUsers );  // ADMIN AUTH
 router.post('/signup', signUp ); // NO AUTH
 router.post('/login', login ); // NO AUTH
-router.get('/user/:id', authRequire, getUserProfile ); // user profile, USER AUTH
+router.get('/userprofile', authRequire, getUserProfile ); // user profile, USER AUTH
 
 export default router
