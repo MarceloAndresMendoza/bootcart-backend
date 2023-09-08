4094a156c2e43e1903fde2a9cafdf16e296f32cf
MarceloAndres <mmendozaf@gmail.com>
2023-09-07 23:12:45 -0300

    fix: deleted username field, since email is the important one

--- models/User.model.js
+++ models/User.model.js
@@ -17,7 +17,6 @@ const cartItemSchema = new Schema({
 
 
 const userSchema = new Schema({
-    username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, required: true, default: 'user'},
